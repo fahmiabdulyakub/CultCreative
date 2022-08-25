c42d4d69b72c910c45d9dd4d47aa1e7d4990dc37
fahmi.yakub.flip <fahmi.yakub@flip.id>
2022-08-25 16:18:59 +0700

    create alias path

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from 'themes';
+import {hp, wp} from 'utils/StyleUtil';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+  map: {
+    height: hp(100),
+  },
+  containerSearch: {
+    position: 'absolute',
+    marginTop: hp(3),
+    marginLeft: wp(5),
+    height: hp(63),
+  },
+  containerItem: {
+    marginTop: hp(2),
+  },
+});
+
+export default styles;
